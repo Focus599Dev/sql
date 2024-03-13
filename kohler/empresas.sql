d1d5c604264bec88ee3eb7b7d480b583a006ff1a
Marlon oliveira barbosa <marlon.academi@hotmail.com>
2024-03-13 08:58:57 -0300

    Create empresas.sql

---
+++ kohler/empresas.sql
@@ -0,0 +1,11 @@
+INSERT INTO `fe_ngtb_empresa_empr` ( `empr_nome`, `empr_razao_social`, `empr_cnpj`, `empr_uf`, `empr_cert_dtval`, `empr_status`, `empr_centro`) VALUES ('Kohler Brazil - Centro 1', 'Kohler Produtos Para Cozinhas e Banheiros Ltda - B1', '20373585000100', 'MG', null, 'A', '');
+
+INSERT INTO `fe_ngtb_empresa_empr` ( `empr_nome`, `empr_razao_social`, `empr_cnpj`, `empr_uf`, `empr_cert_dtval`, `empr_status`, `empr_centro`) VALUES ('Kohler Brazil - Centro 2', 'Kohler Produtos para Cozinhas e Banheiros Ltda - B2', '20373585000445', 'MG', null, 'A', '');
+
+INSERT INTO `fe_ngtb_empresa_empr` ( `empr_nome`, `empr_razao_social`, `empr_cnpj`, `empr_uf`, `empr_cert_dtval`, `empr_status`, `empr_centro`) VALUES ('Kohler Brazil (3PL)', 'Kohler Produtos Para Cozinhas e Banheiros LTDA', '20373585000798', 'SP', null, 'A', '');
+
+INSERT INTO `fe_ngtb_empresa_empr` ( `empr_nome`, `empr_razao_social`, `empr_cnpj`, `empr_uf`, `empr_cert_dtval`, `empr_status`, `empr_centro`) VALUES ('Kohler Brazil Headquarters', 'Kohler Produtos para Cozinhas e Banheiros Ltda - B4', '20373585000526', 'SP', null, 'A', '');
+
+INSERT INTO `fe_ngtb_empresa_empr` ( `empr_nome`, `empr_razao_social`, `empr_cnpj`, `empr_uf`, `empr_cert_dtval`, `empr_status`, `empr_centro`) VALUES ('KOHLER BRAZIL - Showroom', 'Kohler Produtos para Cozinhas e Banheiros Ltda - B5', '20373585000607', 'SP', null, 'A', '');
+
+INSERT INTO `fe_ngtb_empresa_empr` ( `empr_nome`, `empr_razao_social`, `empr_cnpj`, `empr_uf`, `empr_cert_dtval`, `empr_status`, `empr_centro`) VALUES ('Kohler Brazil (3PL)', 'Kohler Produtos Para Cozinhas e Banheiros Ltda - B6', '20373585000879', 'DF', null, 'A', '');
